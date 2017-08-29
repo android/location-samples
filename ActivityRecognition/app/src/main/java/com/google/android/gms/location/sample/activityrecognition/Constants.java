19827b6e8c8d37605743a6700ec9ffe09891f5e7
shailen <shailentuli@google.com>
2017-08-29 13:16:54 -0700

    Updated Activity Recognition sample to connectionless GAC
    Change-Id: I2dc5fa3695047622e7bbb28ef05129d8241503ec

--- ActivityRecognition/app/src/main/java/com/google/android/gms/location/sample/activityrecognition/Constants.java
+++ ActivityRecognition/app/src/main/java/com/google/android/gms/location/sample/activityrecognition/Constants.java
@@ -20,36 +20,33 @@ import android.content.Context;
 import android.content.res.Resources;
 
 import com.google.android.gms.location.DetectedActivity;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 /**
  * Constants used in this sample.
  */
 final class Constants {
 
-    private Constants() {
-    }
+    private Constants() {}
 
-    private static final String PACKAGE_NAME = "com.google.android.gms.location.activityrecognition";
+    private static final String PACKAGE_NAME =
+            "com.google.android.gms.location.activityrecognition";
 
-    static final String BROADCAST_ACTION = PACKAGE_NAME + ".BROADCAST_ACTION";
-
-    static final String ACTIVITY_EXTRA = PACKAGE_NAME + ".ACTIVITY_EXTRA";
-
-    static final String SHARED_PREFERENCES_NAME = PACKAGE_NAME + ".SHARED_PREFERENCES";
-
-    static final String ACTIVITY_UPDATES_REQUESTED_KEY = PACKAGE_NAME +
+    static final String KEY_ACTIVITY_UPDATES_REQUESTED = PACKAGE_NAME +
             ".ACTIVITY_UPDATES_REQUESTED";
 
-    static final String DETECTED_ACTIVITIES = PACKAGE_NAME + ".DETECTED_ACTIVITIES";
+    static final String KEY_DETECTED_ACTIVITIES = PACKAGE_NAME + ".DETECTED_ACTIVITIES";
 
     /**
      * The desired time between activity detections. Larger values result in fewer activity
      * detections while improving battery life. A value of 0 results in activity detections at the
-     * fastest possible rate. Getting frequent updates negatively impact battery life and a real
-     * app may prefer to request less frequent updates.
+     * fastest possible rate.
      */
-    static final long DETECTION_INTERVAL_IN_MILLISECONDS = 0;
-
+    static final long DETECTION_INTERVAL_IN_MILLISECONDS = 30 * 1000; // 30 seconds
     /**
      * List of DetectedActivity types that we monitor in this sample.
      */
@@ -63,31 +60,4 @@ final class Constants {
             DetectedActivity.TILTING,
             DetectedActivity.UNKNOWN
     };
-
-    /**
-     * Returns a human readable String corresponding to a detected activity type.
-     */
-    static String getActivityString(Context context, int detectedActivityType) {
-        Resources resources = context.getResources();
-        switch(detectedActivityType) {
-            case DetectedActivity.IN_VEHICLE:
-                return resources.getString(R.string.in_vehicle);
-            case DetectedActivity.ON_BICYCLE:
-                return resources.getString(R.string.on_bicycle);
-            case DetectedActivity.ON_FOOT:
-                return resources.getString(R.string.on_foot);
-            case DetectedActivity.RUNNING:
-                return resources.getString(R.string.running);
-            case DetectedActivity.STILL:
-                return resources.getString(R.string.still);
-            case DetectedActivity.TILTING:
-                return resources.getString(R.string.tilting);
-            case DetectedActivity.UNKNOWN:
-                return resources.getString(R.string.unknown);
-            case DetectedActivity.WALKING:
-                return resources.getString(R.string.walking);
-            default:
-                return resources.getString(R.string.unidentifiable_activity, detectedActivityType);
-        }
-    }
 }
