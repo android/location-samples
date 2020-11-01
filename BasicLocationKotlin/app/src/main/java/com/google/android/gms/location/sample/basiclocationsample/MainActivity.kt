01be1dd42f335539bf1be2acbe4a34826fee600b
MadRat96 <mischa.alpeew@yandex.ru>
2020-11-01 20:25:37 +0300

    BasicLocationKotlin module: -refactored MainActivity for using ViewBinding instead of findViewById and Java 8 features -replaced deprecated BuildConfig.APPLICATION_ID with BuildConfig.LIBRARY_PACKAGE_NAME -MainActivity: reorganised methods

--- BasicLocationKotlin/app/src/main/java/com/google/android/gms/location/sample/basiclocationsample/MainActivity.kt
+++ BasicLocationKotlin/app/src/main/java/com/google/android/gms/location/sample/basiclocationsample/MainActivity.kt
@@ -19,31 +19,25 @@ package com.google.android.gms.location.sample.basiclocationsample
 import android.Manifest.permission.ACCESS_COARSE_LOCATION
 import android.annotation.SuppressLint
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.content.pm.PackageManager.PERMISSION_GRANTED
-import android.location.Location
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.snackbar.Snackbar.LENGTH_INDEFINITE
-import com.google.android.material.snackbar.Snackbar.LENGTH_LONG
-import androidx.core.app.ActivityCompat
-import androidx.appcompat.app.AppCompatActivity
 import android.util.Log
 import android.view.View
-import android.widget.TextView
-
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.viewbinding.BuildConfig
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.sample.basiclocationsample.BuildConfig.APPLICATION_ID
-import com.google.android.gms.tasks.Task
+import com.google.android.gms.location.sample.basiclocationsample.databinding.MainActivityBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.Snackbar.LENGTH_INDEFINITE
 
 /**
  * Demonstrates use of the Location API to retrieve the last known location for a device.
  */
-class MainActivity : AppCompatActivity() {
-
+class MainActivity: AppCompatActivity() {
     private val TAG = "MainActivity"
     private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
 
@@ -51,20 +45,23 @@ class MainActivity : AppCompatActivity() {
      * Provides the entry point to the Fused Location Provider API.
      */
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-
-    private lateinit var latitudeText: TextView
-    private lateinit var longitudeText: TextView
+    
+    /**
+    * ViewBinding
+     */
+    private lateinit var binding: MainActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-
-        latitudeText = findViewById(R.id.latitude_text)
-        longitudeText = findViewById(R.id.longitude_text)
+        
+        // ViewBinding initialization
+        binding = MainActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
     }
-
+    
     override fun onStart() {
         super.onStart()
 
@@ -74,7 +71,77 @@ class MainActivity : AppCompatActivity() {
             getLastLocation()
         }
     }
-
+    /**
+     * Return the current state of the permissions needed.
+     */
+    private fun checkPermissions() = ActivityCompat.checkSelfPermission(this,
+        ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED
+    private fun requestPermissions() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, ACCESS_COARSE_LOCATION)) {
+            // Provide an additional rationale to the user. This would happen if the user denied the
+            // request previously, but didn't check the "Don't ask again" checkbox.
+            Log.i(TAG, "Displaying permission rationale to provide additional context.")
+            showSnackbar(R.string.permission_rationale, android.R.string.ok) {
+                // Request permission
+                startLocationPermissionRequest()
+            }
+            
+        } else {
+            // Request permission. It's possible this can be auto answered if device policy
+            // sets the permission in a given state or the user denied the permission
+            // previously and checked "Never ask again".
+            Log.i(TAG, "Requesting permission")
+            startLocationPermissionRequest()
+        }
+    }
+    private fun startLocationPermissionRequest() {
+        ActivityCompat.requestPermissions(this, arrayOf(ACCESS_COARSE_LOCATION),
+            REQUEST_PERMISSIONS_REQUEST_CODE)
+    }
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        Log.i(TAG, "onRequestPermissionResult")
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            when {
+                // If user interaction was interrupted, the permission request is cancelled and you
+                // receive empty arrays.
+                grantResults.isEmpty() -> Log.i(TAG, "User interaction was cancelled.")
+                
+                // Permission granted.
+                (grantResults[0] == PERMISSION_GRANTED) -> getLastLocation()
+                
+                // Permission denied.
+                
+                // Notify the user via a SnackBar that they have rejected a core permission for the
+                // app, which makes the Activity useless. In a real app, core permissions would
+                // typically be best requested during a welcome-screen flow.
+                
+                // Additionally, it is important to remember that a permission might have been
+                // rejected without asking the user for permission (device policy or "Never ask
+                // again" prompts). Therefore, a user interface affordance is typically implemented
+                // when permissions are denied. Otherwise, your app could appear unresponsive to
+                // touches or interactions which have required permissions.
+                else -> {
+                    showSnackbar(R.string.permission_denied_explanation, R.string.settings) {
+                        // Build intent that displays the App settings screen.
+                        val intent = Intent().apply {
+                            action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                            data = Uri.fromParts("package",
+                                BuildConfig.LIBRARY_PACKAGE_NAME, null)
+                            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                        }
+                        startActivity(intent)
+                    }
+                }
+            }
+        }
+    }
     /**
      * Provides a simple way of getting a device's location and is well suited for
      * applications that do not require a fine-grained location and that do not need location
@@ -91,9 +158,9 @@ class MainActivity : AppCompatActivity() {
 
                         val location = taskLocation.result
 
-                        latitudeText.text = resources
+                        binding.currentLatitude.text = resources
                                 .getString(R.string.latitude_label, location?.latitude)
-                        longitudeText.text = resources
+                        binding.currentLongitude.text = resources
                                 .getString(R.string.longitude_label, location?.longitude)
                     } else {
                         Log.w(TAG, "getLastLocation:exception", taskLocation.exception)
@@ -101,7 +168,6 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
     }
-
     /**
      * Shows a [Snackbar].
      *
@@ -109,92 +175,13 @@ class MainActivity : AppCompatActivity() {
      * @param actionStrId The text of the action item.
      * @param listener The listener associated with the Snackbar action.
      */
-    private fun showSnackbar(
-        snackStrId: Int,
-        actionStrId: Int = 0,
-        listener: View.OnClickListener? = null
-    ) {
-        val snackbar = Snackbar.make(findViewById(android.R.id.content), getString(snackStrId),
+    private fun showSnackbar(snackStrId: Int, actionStrId: Int = 0,
+                             listener: View.OnClickListener? = null) {
+        val snackBar = Snackbar.make(findViewById(android.R.id.content), getString(snackStrId),
                 LENGTH_INDEFINITE)
         if (actionStrId != 0 && listener != null) {
-            snackbar.setAction(getString(actionStrId), listener)
+            snackBar.setAction(getString(actionStrId), listener)
         }
-        snackbar.show()
+        snackBar.show()
     }
-
-    /**
-     * Return the current state of the permissions needed.
-     */
-    private fun checkPermissions() =
-            ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED
-
-    private fun startLocationPermissionRequest() {
-        ActivityCompat.requestPermissions(this, arrayOf(ACCESS_COARSE_LOCATION),
-                REQUEST_PERMISSIONS_REQUEST_CODE)
-    }
-
-    private fun requestPermissions() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, ACCESS_COARSE_LOCATION)) {
-            // Provide an additional rationale to the user. This would happen if the user denied the
-            // request previously, but didn't check the "Don't ask again" checkbox.
-            Log.i(TAG, "Displaying permission rationale to provide additional context.")
-            showSnackbar(R.string.permission_rationale, android.R.string.ok, View.OnClickListener {
-                // Request permission
-                startLocationPermissionRequest()
-            })
-
-        } else {
-            // Request permission. It's possible this can be auto answered if device policy
-            // sets the permission in a given state or the user denied the permission
-            // previously and checked "Never ask again".
-            Log.i(TAG, "Requesting permission")
-            startLocationPermissionRequest()
-        }
-    }
-
-    /**
-     * Callback received when a permissions request has been completed.
-     */
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        Log.i(TAG, "onRequestPermissionResult")
-        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
-            when {
-                // If user interaction was interrupted, the permission request is cancelled and you
-                // receive empty arrays.
-                grantResults.isEmpty() -> Log.i(TAG, "User interaction was cancelled.")
-
-                // Permission granted.
-                (grantResults[0] == PackageManager.PERMISSION_GRANTED) -> getLastLocation()
-
-                // Permission denied.
-
-                // Notify the user via a SnackBar that they have rejected a core permission for the
-                // app, which makes the Activity useless. In a real app, core permissions would
-                // typically be best requested during a welcome-screen flow.
-
-                // Additionally, it is important to remember that a permission might have been
-                // rejected without asking the user for permission (device policy or "Never ask
-                // again" prompts). Therefore, a user interface affordance is typically implemented
-                // when permissions are denied. Otherwise, your app could appear unresponsive to
-                // touches or interactions which have required permissions.
-                else -> {
-                    showSnackbar(R.string.permission_denied_explanation, R.string.settings,
-                            View.OnClickListener {
-                                // Build intent that displays the App settings screen.
-                                val intent = Intent().apply {
-                                    action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-                                    data = Uri.fromParts("package", APPLICATION_ID, null)
-                                    flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                                }
-                                startActivity(intent)
-                            })
-                }
-            }
-        }
-    }
-
 }
