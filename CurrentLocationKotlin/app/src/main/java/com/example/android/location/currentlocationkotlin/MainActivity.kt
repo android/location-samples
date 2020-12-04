3f3f0eb094d70d76d769fc5d98c96ed8fcb1e0d4
Jeremy Walker <jewalker@google.com>
2020-12-03 22:00:36 -0800

    Add permission code.

--- CurrentLocationKotlin/app/src/main/java/com/example/android/location/currentlocationkotlin/MainActivity.kt
+++ CurrentLocationKotlin/app/src/main/java/com/example/android/location/currentlocationkotlin/MainActivity.kt
@@ -15,12 +15,120 @@
  */
 package com.example.android.location.currentlocationkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.location.currentlocationkotlin.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
+/**
+ * Demonstrates how to easily get the current location.
+ */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    // If the user denied a previous permission request, but didn't check "Don't ask again", this
+    // Snackbar provides an explanation for why user should approve, i.e., the additional rationale.
+    private val fineLocationRationalSnackbar by lazy {
+        Snackbar.make(
+            binding.container,
+            R.string.fine_location_permission_rationale,
+            Snackbar.LENGTH_LONG
+        ).setAction(R.string.ok) {
+            requestPermissions(
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            REQUEST_FINE_LOCATION_PERMISSIONS_REQUEST_CODE
+            )
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        setContentView(view)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        Log.d(TAG, "onRequestPermissionResult()")
+
+        if (requestCode == REQUEST_FINE_LOCATION_PERMISSIONS_REQUEST_CODE) {
+            when {
+                grantResults.isEmpty() ->
+                    // If user interaction was interrupted, the permission request
+                    // is cancelled and you receive an empty array.
+                    Log.d(TAG, "User interaction was cancelled.")
+
+                grantResults[0] == PackageManager.PERMISSION_GRANTED ->
+                    Snackbar.make(
+                        binding.container,
+                        R.string.permission_approved_explanation,
+                        Snackbar.LENGTH_LONG
+                    )
+                        .show()
+
+                else -> {
+                    Snackbar.make(
+                        binding.container,
+                        R.string.fine_permission_denied_explanation,
+                        Snackbar.LENGTH_LONG
+                    )
+                        .setAction(R.string.settings) {
+                            // Build intent that displays the App settings screen.
+                            val intent = Intent()
+                            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                            val uri = Uri.fromParts(
+                                "package",
+                                BuildConfig.APPLICATION_ID,
+                                null
+                            )
+                            intent.data = uri
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                            startActivity(intent)
+                        }
+                        .show()
+                }
+            }
+        }
+    }
+
+    fun locationRequestOnClick(view: View) {
+        Log.d(TAG, "locationRequestOnClick()")
+
+        val permissionApproved =
+            applicationContext.hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (permissionApproved) {
+            requestCurrentLocation()
+        } else {
+            requestPermissionWithRationale(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                REQUEST_FINE_LOCATION_PERMISSIONS_REQUEST_CODE,
+                fineLocationRationalSnackbar)
+        }
+    }
+
+    private fun requestCurrentLocation() {
+        Log.d(TAG, "requestCurrentLocation()")
+        // TODO:
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+
+        private const val REQUEST_FINE_LOCATION_PERMISSIONS_REQUEST_CODE = 34
     }
 }
