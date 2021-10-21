1f6b4d48d5da7cf1385d11c9221165f4a5f33776
Jonathan Koren <jdkoren@google.com>
2021-10-21 14:07:13 -0700

    Check that Google Play Services are available
    Adds a check the Google Play Services are available and introduces UI
    states accordingly. Also adds Hilt and coroutines dependencies.
    
    Bug: 203460543
    Change-Id: Iaeae4f511b7ef8993884f36964e0e9507b8531ea

--- LocationAddress/app/src/main/java/com/google/android/gms/location/sample/locationaddress/MainActivity.kt
+++ LocationAddress/app/src/main/java/com/google/android/gms/location/sample/locationaddress/MainActivity.kt
@@ -19,22 +19,54 @@ package com.google.android.gms.location.sample.locationaddress
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.google.android.gms.location.sample.locationaddress.UiState.Initializing
+import com.google.android.gms.location.sample.locationaddress.UiState.PlayServicesAvailable
+import com.google.android.gms.location.sample.locationaddress.UiState.PlayServicesUnavailable
 import com.google.android.gms.location.sample.locationaddress.ui.theme.LocationAddressTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             LocationAddressTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting(stringResource(R.string.app_name))
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(stringResource(id = R.string.app_name))
+                            }
+                        )
+                    }
+                ) {
+                    MainScreen()
                 }
             }
         }
@@ -42,14 +74,87 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(text: String) {
-    Text(text = text, style = MaterialTheme.typography.h5)
+fun MainScreen(viewModel: MainViewModel = hiltViewModel()) {
+    val uiState by viewModel.uiState.collectAsState()
+    when (uiState) {
+        Initializing -> InitializingScreen()
+        PlayServicesUnavailable -> ServiceUnavailableScreen()
+        PlayServicesAvailable -> GeocoderScreen()
+    }
+}
+
+@Composable
+fun InitializingScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.initializing),
+            style = MaterialTheme.typography.h6,
+            modifier = Modifier
+                .padding(all = 16.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+        CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
+    }
+}
+
+@Composable
+fun ServiceUnavailableScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(id = R.string.play_services_unavailable),
+            style = MaterialTheme.typography.h6,
+            modifier = Modifier
+                .padding(all = 16.dp)
+                .align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center
+        )
+        Icon(
+            Icons.Filled.Warning,
+            tint = MaterialTheme.colors.primary,
+            contentDescription = "",
+            modifier = Modifier.size(48.dp)
+        )
+    }
+}
+
+@Composable
+fun GeocoderScreen() {
+    Button(
+        modifier = Modifier.padding(all = 16.dp),
+        onClick = { /*TODO*/ }
+    ) {
+        Text(text = "Find Address")
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun InitializingScreenPreview() {
+    LocationAddressTheme {
+        InitializingScreen()
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ServiceUnavailableScreenPreview() {
+    LocationAddressTheme {
+        ServiceUnavailableScreen()
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+fun GeocoderScreenPreview() {
     LocationAddressTheme {
-        Greeting("Hello Android")
+        GeocoderScreen()
     }
 }
--- LocationAddress/app/src/main/java/com/google/android/gms/location/sample/locationaddress/ui/theme/Type.kt
+++ LocationAddress/app/src/main/java/com/google/android/gms/location/sample/locationaddress/ui/theme/Type.kt
@@ -17,7 +17,6 @@
 package com.google.android.gms.location.sample.locationaddress.ui.theme
 
 import androidx.compose.material.Typography
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -28,8 +27,7 @@ val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        color = Color.Magenta
+        fontSize = 16.sp
     )
     /* Other default text styles to override
     button = TextStyle(
